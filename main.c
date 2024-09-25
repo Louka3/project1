1d23409a3fc02c4f8633c479615a1f174efc32a0
Louka3 <lkuczykowski@gmail.com>
2024-09-25 14:37:52 -0400

    started the computation method of finding the sizes

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <limits.h>
+#include <math.h> // need the math library in order to use the pow() function
 
 // Write a program to determine the ranges of char, 
 // short, int, and long variables, both signed and unsigned, 
@@ -30,8 +31,29 @@ int main(){
     printf("Minimum value for a unsigned long int: %d\n", 0);
     printf("Maximum value for a unsigned long int: %lu\n", ULONG_MAX);
     printf("-------------\n\n");
-    printf("Using direct computation:\n\n");
 
+    printf("Using direct computation:\n\n");
+    printf("Minimum value for a signed char: %d\n", -(int)pow(2, sizeof(char) * (8 - 1)));
+    printf("Maximum value for a signed char: %d\n", (int) pow(2, sizeof(char) * (8 - 1)) - 1);
+    printf("Minimum value for a unsigned char: %d\n", 0);
+    printf("Maximum value for a unsigned char: %d\n", (int) pow(2, sizeof(char) * 8) - 1);
+    printf("CHAR_MIN: %d\n", CHAR_MIN);
+    printf("CHAR_MAX: %d\n", CHAR_MAX);
+    printf("-------------\n");
+    printf("Minimum value for a short int: %d\n", SHRT_MIN);
+    printf("Maximum value for a short int: %d\n", SHRT_MAX);
+    printf("Minimum value for a unsigned short int: %d\n", 0);
+    printf("Maximum value for a unsigned short int: %d\n", USHRT_MAX);
+    printf("-------------\n");
+    printf("Minimum value for a signed int: %i\n", INT_MIN);
+    printf("Maximum value for a signed int: %i\n", INT_MAX);
+    printf("Minimum value for a unsigned int: %u\n", 0);
+    printf("MAximum value for a unsigned int: %u\n", UINT_MAX);
+    printf("-------------\n");
+    printf("Minimum value for a signed long int: %ld\n", LONG_MIN);
+    printf("Maximum value for a signed long int: %ld\n", LONG_MAX);
+    printf("Minimum value for a unsigned long int: %d\n", 0);
+    printf("Maximum value for a unsigned long int: %lu\n", ULONG_MAX);
     
     return 0;
 }
